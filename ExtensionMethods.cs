5179a55f009f73ed3a88e8f3513bafe640f785d2
Jeremy McLain <gongchengshi@gmail.com>
2016-10-05 16:41:33 -0700

    string.Split

---
+++ ExtensionMethods.cs
@@ -0,0 +1,10 @@
+﻿namespace Treehouse.Common
+{
+    public static class ExtensionMethods
+    {
+        public static bool IsDefault<T>(this T @this)
+        {
+            return @this.Equals(default(T));
+        }
+    }
+}
---
+++ StringExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Treehouse.Common
+{
+    public static class StringExtensions
+    {
+        public static string[] Split(this string @this, char delimiter, int max)
+        {
+            if(@this == null)
+            {
+                throw new ArgumentNullException(nameof(@this));
+            }
+
+            return @this.Split(new[] { delimiter }, max);
+        }
+    }
+}
