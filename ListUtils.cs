73f214c47b6114010ecb72891ef704196ba93599
Jeremy McLain <gongchengshi@gmail.com>
2016-10-05 15:35:09 -0700

    Added this

--- ListUtils.cs
+++ ListUtils.cs
@@ -7,7 +7,7 @@ namespace Treehouse
     {
         private static readonly Random _random = new Random();
 
-        public static T RandomItem<T>(List<T> list)
+        public static T RandomItem<T>(this List<T> list)
         {
             return list[_random.Next(0, list.Count)];
         }
